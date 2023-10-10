a596c1b6ea2ea081c2a5e3aa0aac34a5585ef4b3
Espen Røvik Larsen <espenrl@users.noreply.github.com>
2023-10-10 15:48:47 +0200

    net8 skisharp

---
+++ net8-skiasharp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace net8test;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ net8-skiasharp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace net8test;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
